5e29ce844ada28d400975084fe9336d7ba73c622
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-09 11:00:13 +0100

    add action and reducers

---
+++ src/state/action/index.ts
@@ -0,0 +1,17 @@
+interface DepositAction {
+    type:"deposit",
+    payload:number
+}
+
+interface WithdrawAction {
+    type:"withdraw",
+    payload:number
+}
+
+interface BankruptAction {
+    type:"bankrupt"
+}
+
+type Action = DepositAction | WithdrawAction | BankruptAction
+
+export default Action
